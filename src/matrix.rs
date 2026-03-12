b241819505ca792cc9ff6c115f29e245fb6d554c
Haksell <axel.brisse@gmail.com>
2026-03-12 19:32:53 +0100

    only square matrices

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,8 +1,8 @@
 use std::ops::Index;
 
 #[derive(Clone, Debug, PartialEq)]
-pub struct Matrix<const H: usize, const W: usize> {
-    values: [[f32; W]; H],
+pub struct Matrix<const N: usize> {
+    values: [[f32; N]; N],
 }
 
 #[macro_export]
@@ -12,51 +12,36 @@ macro_rules! m {
     }};
 }
 
-impl<const H: usize, const W: usize> Matrix<H, W> {
-    pub const fn new(values: [[f32; W]; H]) -> Self {
+impl<const N: usize> Matrix<N> {
+    pub const fn new(values: [[f32; N]; N]) -> Self {
         Self { values }
     }
 
     pub const fn zeros() -> Self {
         Self {
-            values: [[0.; W]; H],
+            values: [[0.; N]; N],
         }
     }
 
     pub const fn ones() -> Self {
         Self {
-            values: [[1.; W]; H],
+            values: [[1.; N]; N],
         }
     }
 
     pub const fn full(value: f32) -> Self {
         Self {
-            values: [[value; W]; H],
+            values: [[value; N]; N],
         }
     }
 
     #[expect(clippy::unused_self)]
-    pub const fn height(&self) -> usize {
-        H
-    }
-
-    #[expect(clippy::unused_self)]
-    pub const fn width(&self) -> usize {
-        W
-    }
-
-    #[expect(clippy::unused_self)]
-    pub const fn shape(&self) -> (usize, usize) {
-        (H, W)
-    }
-
-    #[expect(clippy::unused_self)]
-    pub const fn is_square(&self) -> bool {
-        H == W
+    pub const fn size(&self) -> usize {
+        N
     }
 }
 
-impl<const H: usize, const W: usize> Index<(usize, usize)> for Matrix<H, W> {
+impl<const N: usize> Index<(usize, usize)> for Matrix<N> {
     type Output = f32;
 
     fn index(&self, (y, x): (usize, usize)) -> &Self::Output {
@@ -66,10 +51,10 @@ impl<const H: usize, const W: usize> Index<(usize, usize)> for Matrix<H, W> {
 
 macro_rules! impl_matrix_matrix {
     ($lhs:ty, $rhs:ty) => {
-        impl<const H: usize, const W: usize> std::ops::Add<$rhs> for $lhs {
-            type Output = Matrix<H, W>;
+        impl<const N: usize> std::ops::Add<$rhs> for $lhs {
+            type Output = Matrix<N>;
 
-            fn add(self, rhs: $rhs) -> Matrix<H, W> {
+            fn add(self, rhs: $rhs) -> Matrix<N> {
                 Matrix {
                     values: std::array::from_fn(|y| {
                         std::array::from_fn(|x| self.values[y][x] + rhs.values[y][x])
@@ -78,10 +63,10 @@ macro_rules! impl_matrix_matrix {
             }
         }
 
-        impl<const H: usize, const W: usize> std::ops::Sub<$rhs> for $lhs {
-            type Output = Matrix<H, W>;
+        impl<const N: usize> std::ops::Sub<$rhs> for $lhs {
+            type Output = Matrix<N>;
 
-            fn sub(self, rhs: $rhs) -> Matrix<H, W> {
+            fn sub(self, rhs: $rhs) -> Matrix<N> {
                 Matrix {
                     values: std::array::from_fn(|y| {
                         std::array::from_fn(|x| self.values[y][x] - rhs.values[y][x])
@@ -92,15 +77,15 @@ macro_rules! impl_matrix_matrix {
     };
 }
 
-impl_matrix_matrix!(Matrix<H, W>, Matrix<H, W>);
-impl_matrix_matrix!(Matrix<H, W>, &Matrix<H, W>);
-impl_matrix_matrix!(&Matrix<H, W>, Matrix<H, W>);
-impl_matrix_matrix!(&Matrix<H, W>, &Matrix<H, W>);
+impl_matrix_matrix!(Matrix<N>, Matrix<N>);
+impl_matrix_matrix!(Matrix<N>, &Matrix<N>);
+impl_matrix_matrix!(&Matrix<N>, Matrix<N>);
+impl_matrix_matrix!(&Matrix<N>, &Matrix<N>);
 
 macro_rules! impl_matrix_scalar {
     ($matrix:ty) => {
-        impl<const H: usize, const W: usize> std::ops::Mul<f32> for $matrix {
-            type Output = Matrix<H, W>;
+        impl<const N: usize> std::ops::Mul<f32> for $matrix {
+            type Output = Matrix<N>;
 
             fn mul(self, scalar: f32) -> Self::Output {
                 Matrix {
@@ -111,8 +96,8 @@ macro_rules! impl_matrix_scalar {
             }
         }
 
-        impl<const H: usize, const W: usize> std::ops::Div<f32> for $matrix {
-            type Output = Matrix<H, W>;
+        impl<const N: usize> std::ops::Div<f32> for $matrix {
+            type Output = Matrix<N>;
 
             fn div(self, scalar: f32) -> Self::Output {
                 self * (1.0 / scalar)
@@ -121,8 +106,8 @@ macro_rules! impl_matrix_scalar {
     };
 }
 
-impl_matrix_scalar!(Matrix<H, W>);
-impl_matrix_scalar!(&Matrix<H, W>);
+impl_matrix_scalar!(Matrix<N>);
+impl_matrix_scalar!(&Matrix<N>);
 
 #[expect(clippy::float_cmp)]
 #[cfg(test)]
@@ -136,27 +121,25 @@ mod tests {
             m![[1., 2.], [3.14, 4.2]]
         );
         assert_eq!(
-            Matrix::<3, 2>::zeros().values,
-            [[0., 0.], [0., 0.], [0., 0.]]
+            Matrix::<3>::zeros().values,
+            [[0., 0., 0.], [0., 0., 0.], [0., 0., 0.]]
         );
-        assert_eq!(Matrix::<2, 3>::ones(), Matrix::full(1.));
+        assert_eq!(Matrix::<2>::ones(), Matrix::full(1.));
     }
 
     #[test]
     fn test_index() {
-        let m = m![[0.25, 0.5, 1.], [2., 4., 8.]];
+        let m = m![[0.25, 0.5], [2., 4.]];
         assert_eq!(m[(0, 0)], 0.25);
         assert_eq!(m[(0, 1)], 0.5);
-        assert_eq!(m[(0, 2)], 1.);
         assert_eq!(m[(1, 0)], 2.);
         assert_eq!(m[(1, 1)], 4.);
-        assert_eq!(m[(1, 2)], 8.);
     }
 
     #[test]
     #[should_panic]
     fn test_index_invalid_row() {
-        let m = m![[0.25, 0.5, 1.], [2., 4., 8.]];
+        let m = m![[0.25, 0.5], [2., 4.]];
         #[expect(clippy::no_effect)]
         m[(2, 0)];
     }
@@ -164,64 +147,62 @@ mod tests {
     #[test]
     #[should_panic]
     fn test_index_invalid_col() {
-        let m = m![[0.25, 0.5, 1.], [2., 4., 8.]];
+        let m = m![[0.25, 0.5], [2., 4.]];
         #[expect(clippy::no_effect)]
         m[(0, 4)];
     }
 
     #[test]
-    fn test_shape() {
-        let m = Matrix::<2, 3>::zeros();
-        assert_eq!(m.height(), 2);
-        assert_eq!(m.width(), 3);
-        assert_eq!(m.shape(), (2, 3));
-    }
-
-    #[test]
-    fn test_is_square() {
-        assert!(!Matrix::<2, 3>::zeros().is_square());
-        assert!(Matrix::<6, 6>::zeros().is_square());
+    fn test_size() {
+        assert_eq!(Matrix::<3>::zeros().size(), 3);
     }
 
     #[test]
     fn test_add() {
         assert_eq!(
-            Matrix::<4, 2>::zeros() + Matrix::<4, 2>::zeros(),
-            Matrix::<4, 2>::zeros()
+            Matrix::<2>::zeros() + Matrix::<2>::zeros(),
+            Matrix::<2>::zeros()
         );
         assert_eq!(
             m![[1., 2.], [3., 4.]] + &m![[-1., -2.], [-3., -4.]],
             Matrix::zeros()
         );
         assert_eq!(
-            &m![[0.5, 1., 1.5], [2., 2.5, 3.]] + m![[2.5, 2., 1.5], [1., 0.5, 0.]],
+            &m![[0.5, 1.], [2., 2.5]] + m![[2.5, 2.], [1., 0.5]],
             Matrix::full(3.)
         );
-        assert_eq!(&m![[1., 2.5, 0.]] + &m![[3., -4., 0.]], m![[4., -1.5, 0.]]);
+        assert_eq!(
+            &m![[1., 2.5, 0.], [1., 2.5, 0.], [1., 2.5, 0.],]
+                + &m![[3., -4., 0.], [3., -4., 0.], [3., -4., 42.]],
+            m![[4., -1.5, 0.], [4., -1.5, 0.], [4., -1.5, 42.],]
+        );
     }
 
     #[test]
     fn test_sub() {
         assert_eq!(
-            Matrix::<4, 2>::zeros() - Matrix::<4, 2>::zeros(),
-            Matrix::<4, 2>::zeros()
+            Matrix::<2>::zeros() - Matrix::<2>::zeros(),
+            Matrix::<2>::zeros()
         );
         assert_eq!(
             m![[1., 2.], [3., 4.]] - &m![[1., 2.], [3., 4.]],
             Matrix::zeros()
         );
         assert_eq!(
-            &m![[0.5, 1., 1.5], [2., 2.5, 3.]] - m![[1.5, 2., 2.5], [3., 3.5, 4.]],
+            &m![[0.5, 1.], [2., 2.5]] - m![[1.5, 2.], [3., 3.5]],
             Matrix::full(-1.)
         );
-        assert_eq!(&m![[1., 2.5, 0.]] - &m![[3., -4., 0.]], m![[-2., 6.5, 0.]]);
+        assert_eq!(
+            &m![[1., 2.5, 0.], [1., 2.5, 0.], [1., 2.5, 0.]]
+                - &m![[3., -4., 0.], [3., -4., 0.], [3., -4., 42.]],
+            m![[-2., 6.5, 0.], [-2., 6.5, 0.], [-2., 6.5, -42.],]
+        );
     }
 
     #[test]
     fn test_scalar_mul() {
-        assert_eq!(Matrix::<4, 2>::zeros() * 7., Matrix::zeros());
-        assert_eq!(m![[1., 2.]] * 3., m![[3., 6.]]);
-        assert_eq!(&m![[1.], [2.]] * -2.5, m![[-2.5], [-5.]]);
-        assert_eq!(&Matrix::<4, 2>::full(2.5) * 2.5, Matrix::full(6.25));
+        assert_eq!(Matrix::<2>::zeros() * 7., Matrix::zeros());
+        assert_eq!(m![[1., 2.], [3., 4.]] * 3., m![[3., 6.], [9., 12.]]);
+        assert_eq!(&Matrix::<4>::full(2.5) * 2.5, Matrix::full(6.25));
     }
 }
